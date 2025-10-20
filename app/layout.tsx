09e99dea3b3e49c6059bb0d3386f0b96f0f1eaae
diegobarrionuevo1 <diegobarrionuevo11@gmail.com>
2025-10-20 18:04:21 -0300

    cambio: actualizar el título del sitio web a "Grupo Jugando" y el favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "GoldenBot",
+  title: "Grupo Jugando",
   description: "Jugá tranquilo, nosotros te representamos.",
 };
 
